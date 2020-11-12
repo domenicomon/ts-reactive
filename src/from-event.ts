ca99e08f81d4dc4b9b4dd0ba2f178fc4c5feaf3d
domenico.montalbano@deltatre.it <domenico.montalbano@deltatre.it>
2020-11-12 10:12:11 +0100

    fromEvents returning always Event Observable

--- src/from-event.ts
+++ src/from-event.ts
@@ -1,12 +1,12 @@
 import { Handler, Observable } from "./observable";
 
-export function fromEvent<T>(element: HTMLElement, eventName: string, listenerOptions?: any): Observable<T> {
-    const obs = new Observable<T>();
+export function fromEvent(element: HTMLElement, eventName: string, listenerOptions?: any): Observable<Event> {
+    const obs = new Observable<Event>();
     const eventListener = (event: Event) => {
-        obs.update(event as unknown as T);
+        obs.update(event as unknown as Event);
     }
     element.addEventListener(eventName, eventListener, listenerOptions);
-    obs.unsubscribe = (handler: Handler<T>) => {
+    obs.unsubscribe = (handler: Handler<Event>) => {
         obs.unsubscribe(handler);
         element.removeEventListener(eventName, eventListener, listenerOptions);
     };
