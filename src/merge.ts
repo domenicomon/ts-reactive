2b8da61efb1461ba360e35be5452addad73f639f
domenico.montalbano@deltatre.it <domenico.montalbano@deltatre.it>
2020-11-12 10:06:29 +0100

    Fixed merge type definition

--- src/merge.ts
+++ src/merge.ts
@@ -1,14 +1,14 @@
 import { Handler, Observable, Subscription } from "./observable";
 
-export function merge<A, B>(streams: [Observable<A>, Observable<B>]): Observable<[A | B]>
-export function merge<A, B, C>(streams: [Observable<A>, Observable<B>, Observable<C>]): Observable<[A | B | C]>
-export function merge<A, B, C, D>(streams: [Observable<A>, Observable<B>, Observable<C>, Observable<D>]): Observable<[A | B | C | D]>
-export function merge<A, B, C, D, E>(streams: [Observable<A>, Observable<B>, Observable<C>, Observable<D>, Observable<E>]): Observable<[A | B | C | D | E]>
-export function merge<A, B, C, D, E, F>(streams: [Observable<A>, Observable<B>, Observable<C>, Observable<D>, Observable<E>, Observable<F>]): Observable<[A | B | C | D | E | F]>
+export function merge<A, B>(streams: [Observable<A>, Observable<B>]): Observable<A | B>
+export function merge<A, B, C>(streams: [Observable<A>, Observable<B>, Observable<C>]): Observable<A | B | C>
+export function merge<A, B, C, D>(streams: [Observable<A>, Observable<B>, Observable<C>, Observable<D>]): Observable<A | B | C | D>
+export function merge<A, B, C, D, E>(streams: [Observable<A>, Observable<B>, Observable<C>, Observable<D>, Observable<E>]): Observable<A | B | C | D | E>
+export function merge<A, B, C, D, E, F>(streams: [Observable<A>, Observable<B>, Observable<C>, Observable<D>, Observable<E>, Observable<F>]): Observable<A | B | C | D | E | F>
 
 // this is type-unsafe but typesafety is ensured above :)
-export function merge(streams: any[]): Observable<any[]> {
-    const stream = new Observable(streams.map(v => v.value));
+export function merge<T>(streams: any[]): Observable<T> {
+    const stream = new Observable<T>();
 
     streams.forEach((s, i) => {
         const subs: Subscription<any>[] = [];
