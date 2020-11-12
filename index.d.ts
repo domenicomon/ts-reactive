6ff9360f6befaf38e9fe59be7452089e832f7d13
domenico.montalbano@deltatre.it <domenico.montalbano@deltatre.it>
2020-11-12 15:55:56 +0100

    Moved index

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export { combineLatest } from './dist/combine-latest';
-export { fromEvent } from './dist/from-event';
-export { merge } from './dist/merge';
-export { Handler, Observable, Subscription } from './dist/observable';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { combineLatest } from './combine-latest';
+export { fromEvent } from './from-event';
+export { merge } from './merge';
+export { Handler, Observable, Subscription } from './observable';
+
